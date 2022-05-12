f4a853bee2cb2af50ffcdac8c9952d63cf13c2d9
Kevin <tingtzuhao@gmail.com>
2022-05-11 17:07:05 -0700

    renamed old files

--- 1. Two Sum.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in nums and nums.index(complement) != i:
-                return [i, nums.index(complement)]
