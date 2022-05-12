a677b1a6fb51e044d55a6e0e70c6a95eb5c77540
Kevin <tingtzuhao@gmail.com>
2022-05-11 17:00:07 -0700

    rename with difficulty

---
+++ 1. (Easy) Two Sum.py
@@ -0,0 +1,6 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in nums and nums.index(complement) != i:
+                return [i, nums.index(complement)]
