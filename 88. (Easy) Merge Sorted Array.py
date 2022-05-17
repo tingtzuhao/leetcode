1a45cbc1d8d32f734cbb494b9ce022cd07278aab
Kevin <tingtzuhao@gmail.com>
2022-05-16 19:46:42 -0700

    modify solution

--- 88. (Easy) Merge Sorted Array.py
+++ 88. (Easy) Merge Sorted Array.py
@@ -1,10 +1,30 @@
-nums1 = [1,2,3,0,0,0]
-nums2 = [2,5,6]
-# -> nums1 = [1,2,2,3,4]
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = m - 1
+        p2 = n - 1
+        lastD = m + n - 1
+        while p1 > -1 and p2 > -1:
+            if nums1[p1] > nums2[p2]:
+                nums1[lastD] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[lastD] = nums2[p2]
+                p2 -= 1
+            lastD -= 1
+        while p2 > -1:
+            nums1[lastD] = nums2[p2]
+            p2 -= 1
+            lastD -= 1
 
-p = 0
-for i in range(len(nums1) - len(nums2), len(nums1)):
-    nums1[i] = nums2[p]
-    p += 1
-nums1 = sorted(nums1)
-print(nums1)
+# Pythonic
+# class Solution:
+#     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+#         """
+#         Do not return anything, modify nums1 in-place instead.
+#         """
+#         for i in range(n):
+#             nums1[i+m] = nums2[i]
+#         nums1.sort()
