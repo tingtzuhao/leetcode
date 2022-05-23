5d8f565c8121a83e45588c34ab290a7ff0a0b8cd
Kevin <tingtzuhao@gmail.com>
2022-05-23 16:05:04 -0700

    add solution

---
+++ 121. (Easy) Best Time to Buy and Sell Stock.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 0 # left = buy; right = sell;
+        maxProfit = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxProfit = max(maxProfit, profit)
+            else:
+                l = r
+            r += 1
+        return maxProfit
